baac3a28d57425464daa3f79f8074f6496f33751
Christopher Fong <christopher.fong115@gmail.com>
2023-01-19 14:06:52 -0500

    added link to each grid item

--- components/ProductGridItem.tsx
+++ components/ProductGridItem.tsx
@@ -2,31 +2,34 @@ import React from "react";
 import { addToCart, Product } from "../redux/cart/cartSlice";
 import { useAppDispatch } from "../redux/hooks";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 const ProductGridItem = ({ item }: { item: Product }) => {
   const dispatch = useAppDispatch();
   return (
-    <motion.div
-      whileHover={{ scale: 1.03 }}
-      className="p-2 text-center w-fit h-[100%] outline outline-slate-600"
-    >
-      <img
-        className="aspect-square object-cover transition hover:duration-150 hover:object-contain"
-        src={item.imageLink[0]}
-      />
-      <div
-        className="w-fit cursor-pointer mx-auto"
-        onClick={(e) => {
-          e.preventDefault();
-          dispatch(addToCart(item));
-        }}
+    <Link href={`/products/${item.productid}`}>
+      <motion.div
+        whileHover={{ scale: 1.03 }}
+        className="p-2 text-center w-fit h-[100%] outline outline-slate-600"
       >
-        +
-      </div>
-      <div>{item.productName}</div>
-      <div>{item.seller}</div>
-      <div>{`$${item.price}`}</div>
-    </motion.div>
+        <img
+          className="aspect-square object-cover transition hover:duration-150 hover:object-contain"
+          src={item.imageLink[0]}
+        />
+        <div
+          className="w-fit cursor-pointer mx-auto"
+          onClick={(e) => {
+            e.preventDefault();
+            dispatch(addToCart(item));
+          }}
+        >
+          +
+        </div>
+        <div>{item.productName}</div>
+        <div>{item.seller}</div>
+        <div>{`$${item.price}`}</div>
+      </motion.div>
+    </Link>
   );
 };
 
